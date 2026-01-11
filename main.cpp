96f7cee77d76bdb601903500c14fc90d0d673eea
Andrea <andreaorozco447@gmail.com>
2026-01-11 10:42:49 -0800

    Assignment 1
    Assignment 1 questions and answers

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Name: Andrea Orozco" << std::endl;
+	std::cout << "Goals in life: to become a bioinformatics software engineer" << std::endl;
+	std::cout << "Hobbies: reading, playing rpg games, and puzzles" << std::endl;
+	std::cout << "Current Interestes: strategic games, reading, and overanalyzing moives and tv shows" << std::endl;
+	std::cout << "where is see myself in 1,3,5,and 7 years:" << std::endl;
+	std::cout << "1 year: finishing my degree and secure an entry-level role" << std::endl;
+	std::cout << "3 years: graduate fully and establish myself in a mid-level position" << std::endl;
+	std::cout << "5 years: working as a bioinformatics software engineer" << std::endl;
+	std::cout << "7 years: working as a senior bioinformatics software engineer" << std::endl;
+	return 0;
+}
